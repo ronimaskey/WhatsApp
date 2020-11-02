dd7864aed905270e1fed744bc1ebd65ca7d3efa9
Roni Maskey <ronimaskey@gmail.com>
2020-11-02 23:22:36 +0100

    [added]sidebar/chat component

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hi</h1>
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbXR2o1wPt78JQpYMju-HSbkCUo8SqF5U",
+    authDomain: "whatsapp-d1b19.firebaseapp.com",
+    databaseURL: "https://whatsapp-d1b19.firebaseio.com",
+    projectId: "whatsapp-d1b19",
+    storageBucket: "whatsapp-d1b19.appspot.com",
+    messagingSenderId: "1013218844200",
+    appId: "1:1013218844200:web:d6bfd435e9cd4f76841f1d"
+  };
