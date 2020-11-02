61eda1f25564be0b417aff0b0c933582b8120ce8
Roni Maskey <ronimaskey@gmail.com>
2020-11-03 00:12:34 +0100

    [edited]sidebar

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import './Sidebar.css'
+import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import {Avatar, IconButton} from '@material-ui/core';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <div className="sidebar_header">
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
