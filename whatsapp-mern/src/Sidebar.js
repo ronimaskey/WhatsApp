48e3179c53553de0fd5c61dadbff8cabddc66d1a
Roni Maskey <ronimaskey@gmail.com>
2020-11-03 12:11:15 +0100

    [setting]sidebar chat/search

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -2,19 +2,45 @@ import React from 'react'
 import './Sidebar.css';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import {Avatar, IconButton} from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {SearchOutlined} from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
+                <Avatar src="https://lh3.googleusercontent.com/ogw/ADGmqu_oVoWtIk-QLjCWF4M_t0vVhAAG9ebmyYLFQFUJzD4=s32-c-mo" />
                 <div className="sidebar_headerRight">
                     <IconButton>
                         <DonutLargeIcon />
                     </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
             </div>
+
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar_chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
         </div>
-    )
+
+    );
 }
 
-export default Sidebar
+export default Sidebar;
