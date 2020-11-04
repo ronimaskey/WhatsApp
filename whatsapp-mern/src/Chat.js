5131ece078fa437bab78cff0a042c5c3217f2936
Roni Maskey <ronimaskey@gmail.com>
2020-11-04 11:50:48 +0100

    [working] in Chat component

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './Chat.css'
 import {SearchOutlined, MoreVert} from '@material-ui/icons';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
  
 function Chat() {
     return (
@@ -27,6 +29,33 @@ function Chat() {
                 </div>
             </div>
             
+            <div className="chat_body">
+                <p className="chat_message">
+                    <span className="chat_name">Sara</span>
+                    This is a message
+                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat_message chat_receiver">
+                    <span className="chat_name">Mink</span>
+                    This is a message
+                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+
+            <div className="chat_footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input 
+                        placeholder="Type a message"
+                        type="text"
+                        />
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     )
 }
