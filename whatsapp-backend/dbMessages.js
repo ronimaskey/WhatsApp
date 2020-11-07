7c08af79dd91d99e7263818ce66431f75b1652b8
ronimaskey <44787252+ronimaskey@users.noreply.github.com>
2020-11-07 01:20:40 +0100

    [added]schema mongoose

--- whatsapp-backend/dbMessages.js
+++ whatsapp-backend/dbMessages.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const whatsappSchema = mongoose.Schema({
-    message: String,
-    name: String,
-    timeStamp: String,
-    received: Boolean
+  message: String,
+  name: String,
+  timestamp: String,
+  received: Boolean,
 });
 
-export default mongoose.model('messageContent', whatsappSchema);
-
+//collection
+export default mongoose.model("messagecontents", whatsappSchema);
--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,35 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import Messages from './dbMessages.js';
+// importing
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 const app = express()
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const connection_url = "mongodb+srv://admin:KGafnOvWv5FPBJfM@cluster0.zkfcl.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 
-mongoose.connect('connection_url', {
+mongoose.connect(connection_url, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+  });
 
-app.get('/', (req,res) => res.status(200).send('hi'));
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
 
-app.post("/messages/new", (req,res) => {
-    const dbMessage = req.body
-
+app.post("/messages/new", (req, res) => {
+    const dbMessage = req.body;
     Messages.create(dbMessage, (err, data) => {
-        if (err) {
-            res.status(500).send(err)
-        } else {
-            res.status(201).send(data)
-        }
-    })
-})
-
-
-
-app.listen(port, ()=>console.log(`Listening on localhost:${port}`))
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(201).send(data);
+      }
+    });
+  });
+  
+  // listen
+  app.listen(port,() => console.log(`Listening on localhost:${port}`));
