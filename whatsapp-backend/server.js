f3d4f3592fea92d7f1a5662603adaf50533f53a8
ronimaskey <ronimaskey@gmail.com>
2020-11-09 12:44:07 +0100

    [created]api for giveback all message

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -46,16 +46,7 @@ mongoose.connect(connection_url, {
 // api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
 
-// api routes that gives back all messages
-app.get("/messages/sync", (req, res) => {
-  Messages.find((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(data);
-    }
-  });
-});
+
 
 app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
